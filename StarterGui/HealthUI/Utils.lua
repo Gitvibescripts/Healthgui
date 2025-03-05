f891e47ecb2c5b217d5da3dfcf422e1f50d6029d
Gitvibescripts <wroomsfromsvibes@gmail.com>
2025-03-05 18:27:49 +0800

    Update Utils.lua

--- StarterGui/HealthUI/Utils.lua
+++ StarterGui/HealthUI/Utils.lua
@@ -1,1 +1,25 @@
+local Utils = {}
+
+function Utils.getHealthStatus(health, maxHealth)
+    local healthPercentage = health / maxHealth
+    
+    if healthPercentage <= 0.25 then
+        return 1 -- Critical
+    elseif healthPercentage <= 0.5 then
+        return 2 -- Low
+    else
+        return 3 -- Healthy
+    end
+end
+
+function Utils.formatNumber(number)
+    return math.floor(number * 10) / 10
+end
+
+function Utils.calculateHealthBarSize(health, maxHealth, maxWidth)
+    local percentage = health / maxHealth
+    return UDim2.new(0, maxWidth * percentage, 1, 0)
+end
+
+return Utils
 
